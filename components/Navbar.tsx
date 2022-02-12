741b37d1ea3250dcab4526454aac8898d7777fc3
pramit-marattha <pramitmarattha5497@gmail.com>
2022-02-12 23:02:05 +0545

    Update navbar menu items

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 function Navbar() {
   return (
     <>
-      <header>
-        <div>
+      <header className="mx-auto flex max-w-7xl justify-between p-5">
+        <div className="flex items-center space-x-10">
           <Link href="/">
             <img
               className="w-44 cursor-pointer object-contain"
@@ -12,8 +12,18 @@ function Navbar() {
               alt="Drraafvt Logo"
             />
           </Link>
+          <div className="item-center hidden space-x-7 md:inline-flex">
+            <h3>Home</h3>
+            <h3>About</h3>
+            <h3>Contact</h3>
+          </div>
         </div>
-        <div></div>
+        <div className="flex items-center space-x-5">
+          <h3>Newsletter</h3>
+          <h3 className="rounded-full bg-teal-500 px-5 py-1 text-white">
+            Get started !
+          </h3>
+        </div>
       </header>
     </>
   )
