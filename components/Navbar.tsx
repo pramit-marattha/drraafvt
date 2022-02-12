fc8afd66ee15dfcf5404ac094ba88a0652be061b
pramit-marattha <pramitmarattha5497@gmail.com>
2022-02-12 22:27:38 +0545

    Update drraafvt logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,22 @@
+import Link from 'next/link'
+
 function Navbar() {
-  return <div>Drraafvt</div>
+  return (
+    <>
+      <header>
+        <div>
+          <Link href="/">
+            <img
+              className="w-44 cursor-pointer object-contain"
+              src="https://user-images.githubusercontent.com/37651620/153719941-9a1dc136-8cc6-4823-8096-548d4a203b45.png"
+              alt="Drraafvt Logo"
+            />
+          </Link>
+        </div>
+        <div></div>
+      </header>
+    </>
+  )
 }
 
 export default Navbar
