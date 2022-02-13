dffc9fae2049a4b4bdb213ff6b86df8e41190d02
pramit-marattha <pramitmarattha5497@gmail.com>
2022-02-13 13:13:54 +0545

    Implement Nextjs server side render

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import { sanityClient, urlFor } from '../sanityConfig.js'
+import { Post } from '../typing'
 
-export default function Home() {
+interface Props {
+  posts: Post[]
+}
+
+export default function Home({ posts }: Props) {
+  console.log(posts)
   return (
     <div className="mx-auto max-w-7xl">
       <Head>
@@ -13,4 +20,27 @@ export default function Home() {
       <Hero />
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = `
+  *[_type == "post"]{
+    _id,
+    title,
+    slug,
+    author ->{
+    name,
+    image
+  },
+  description,
+  mainImage
+  }
+  `
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
