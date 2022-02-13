c38bfcb914250bfea6b25698ad44a8e8b2492b81
pramit-marattha <pramitmarattha5497@gmail.com>
2022-02-13 12:59:17 +0545

    Add description schema

--- drraafvtbackend/schemas/post.js
+++ drraafvtbackend/schemas/post.js
@@ -6,6 +6,12 @@ export default {
     {
       name: 'title',
       title: 'Title',
+      description: 'Give a short title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
       type: 'string',
     },
     {
