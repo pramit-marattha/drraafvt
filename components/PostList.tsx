9c479b6a31a4199e893f6644513fefda022e077e
pramit-marattha <pramitmarattha5497@gmail.com>
2022-02-27 19:43:20 +0545

    Fix postlists

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -66,33 +66,6 @@ const PostList = ({ posts }: Props) => {
               </div>
             </div>
           </div>
-          {
-            //   <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
-            //   {posts.map((post) => (
-            //     <Link key={post._id} href={`/post/${post.slug.current}`}>
-            //       <div className="group cursor-pointer overflow-hidden rounded-lg border-2 border-gray-300/100 shadow-md hover:shadow-xl">
-            //         <img
-            //           className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
-            //           src={urlFor(post.mainImage).url()!}
-            //         />
-            //         <div className="flex justify-between p-5">
-            //           <div>
-            //             <p className="text-lg font-bold">{post.title}</p>
-            //             <p className="text-xs">
-            //               {post.description} by {post.author.name}
-            //             </p>
-            //           </div>
-            //           <img
-            //             className="h-12 w-12 rounded-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
-            //             src={urlFor(post.author.image).url()!}
-            //             alt=""
-            //           />
-            //         </div>
-            //       </div>
-            //     </Link>
-            //   ))}
-            // </div>
-          }
         </Link>
       ))}
     </>
